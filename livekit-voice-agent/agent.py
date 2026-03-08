e889fcc803547fe4da66a7b43fe121efa18affb1
anujsb <anujsbhuyar@gmail.com>
2026-03-08 23:02:02 +0530

    fixed connectivity.

--- livekit-voice-agent/agent.py
+++ livekit-voice-agent/agent.py
@@ -29,7 +29,6 @@ async def my_agent(ctx: agents.JobContext):
         room_options=room_io.RoomOptions(
             audio_input=room_io.AudioInputOptions(
                 noise_cancellation=lambda params: noise_cancellation.BVCTelephony() if params.participant.kind == rtc.ParticipantKind.PARTICIPANT_KIND_SIP else noise_cancellation.BVC(),
-                video_enabled=True, #screen sharing
             ),
         ),
     )
